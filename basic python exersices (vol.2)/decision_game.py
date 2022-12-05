fd903e66f66312dab56564f532eb05dafb813163
Paris Charalampidis <curlycode@outlook.com>
2022-12-06 00:48:39 +0200

    new folder created

---
+++ basic python exersices (vol.2)/decision_game.py
@@ -0,0 +1,46 @@
+print ("You enter a dark room with two doors. Do you go through dor #1 or door #2?")
+
+def enterDoor():
+        door = input( "> ")  
+        
+       
+            
+        if door == "1":
+            print ("There's a giant bear here eating a cheese cake. What do you do?")
+            print("1.Take the cake.")
+            print("2.Scream at the bear.")
+            
+            bear()
+        elif door == "2":
+            print ("You stare into the endless abyss at Cthulhu's retina.")
+            print ("1. Blueberries.")
+            print ("2. Yellow jacket clothespins.")
+            print ("3. Understanding revolvers yelling melodies.")
+
+
+        insanity = input("> ")
+
+        if insanity == "1" or insanity == "2":
+            print  ( "Your body survives powered by a mind of jello. Good job")
+        else:
+            print ("The insanity rots your eyes into a pool of muck. Good job") 
+            
+
+        
+enterDoor()
+
+
+
+def bear():
+    bear = input("> ")
+    if bear == "1":
+        print("The bear eats your face off. Good job!")
+    elif bear == "2":
+        print("The bear eats your legs off. Good job!")
+    else:
+        print( "Well, doing %s is probably better. Bears runs away." % (bear))
+
+
+
+
+  
