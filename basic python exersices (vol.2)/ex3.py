64d38d885e232d55423edc3325180e45b081254f
Paris Charalampidis <curlycode@outlook.com>
2022-12-07 22:53:17 +0200

    adding new exercises

---
+++ basic python exersices (vol.2)/ex3.py
@@ -0,0 +1,12 @@
+#loops and lists
+#while loop 
+
+count = 0
+
+while (count < 3):
+    count = count + 1
+    print("Hello Geek!")
+#using else statement whith while loops
+else:
+    print("In else block")
+                                                                                                                                                                                                                                             
---
+++ basic python exersices (vol.2)/ex4.py
@@ -0,0 +1,14 @@
+n = 4 
+
+for i in range( 0, n ):
+    print(i)
+
+#iterating over a list
+print("List Iteration")
+
+l = ["geeks", "for", "geeks"]
+
+for i in l:
+    print(i)
+
+#iterating over a tuple
