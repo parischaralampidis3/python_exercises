251ea7f5a138574f4c89dbb324fe4ee5a2a07922
Paris Charalampidis <curlycode@outlook.com>
2022-12-08 21:46:54 +0200

    adding exercises

--- basic python exersices (vol.2)/ex3.py
+++ basic python exersices (vol.2)/ex3.py
@@ -1,12 +1,24 @@
-#loops and lists
-#while loop 
+the_count = [ 1,2,3,4,5 ]
+fruits = [ "pears", "apple", "orange", "banana" ]
+change = [ 1,"penies", 2, "dimes", 3, "quarters"]
 
-count = 0
 
-while (count < 3):
-    count = count + 1
-    print("Hello Geek!")
-#using else statement whith while loops
-else:
-    print("In else block")
-                                                                                                                                                                                                                                             
+#this first kind of for loop goes through a list
+
+
+for number in the_count:
+    print("This is count :%d "% number)
+ 
+
+#same as above
+
+for fruit in fruits:
+    print("A fruit type of : %s"% fruit)
+
+#also we can go through mixed lists too
+#n notice we have to use %r since we don't know what's in it.
+
+
+for i in change:
+    print("i ve got %r "% i)
+
--- basic python exersices (vol.2)/ex4.py
+++ basic python exersices (vol.2)/ex4.py
@@ -1,3 +1,18 @@
+
+#loops and lists
+#while loop 
+
+count = 0
+
+while (count < 3):
+    count = count + 1
+    print("Hello Geek!")
+#using else statement whith while loops
+else:
+    print("In else block")
+                             
+
+
 n = 4 
 
 for i in range( 0, n ):
@@ -12,3 +27,39 @@ for i in l:
     print(i)
 
 #iterating over a tuple
+print("Tuple Iteration")
+t = ("geeks", "for", "geeks")
+
+for i in l:
+    print(t)
+
+
+#iterating over a string 
+print("String Iteration")
+s = "Geeks" 
+
+for i in s :
+    print(i)
+
+
+#iterating over a dictionary
+
+#dict() -> new empty dictionary dict(mapping)
+#  -> new dictionary initialized from a mapping object's
+#(key, value) pairs
+print("\nDictionary Iteration")
+d = dict()
+
+d['xyz'] = 123
+d['abc'] = 345
+
+for  i in d:
+    print( "%s" "%d" % (i, d [i]))
+
+
+#iterating over a set 
+print ( "\nSet Iteration")
+
+set1 = { 1,2,3,4,5,6 }
+for i in set1:
+    print(i)
