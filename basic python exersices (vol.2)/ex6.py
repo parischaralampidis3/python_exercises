050b42b27ecbf1b899cbeb86680006c171ff6907
Paris Charalampidis <curlycode@outlook.com>
2022-12-14 05:38:30 +0200

    learning about dictionary in python

---
+++ basic python exersices (vol.2)/ex6.py
@@ -0,0 +1,25 @@
+#Dictionaries
+
+Dict = {1: 'Geeks', 2: 'For', 3: 'Geeks'}
+print(Dict)
+
+
+
+Dict = {'Name': 'Geeks', 1:[1, 2, 3, 4]}
+print("\nDictionary with the use of Mixed Kes:")
+print (Dict)
+
+
+#Creating a Dictionary.
+#with dict() method
+
+Dict = dict( {1: 'Geeks', 2 : 'For', 3: 'Geeks'})
+print("\nDictionary with the use of dict(): ")
+print(Dict)
+
+#Creating a Dictionary
+#with each items as a Pair
+
+Dict = dict([(1,'Geeks'),(2,'For')])
+print("\nDictionary with each item as a pair: ")
+print(Dict)
