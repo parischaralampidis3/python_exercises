4eb913813d996d70efed9cc24a6a7d13c66c3996
Paris Charalampidis <curlycode@outlook.com>
2022-12-14 23:40:44 +0200

    i create a guess random number game

---
+++ basic python exersices/random_quiz.py
@@ -0,0 +1,27 @@
+import random 
+print( "Guess a number from 1-10..")
+
+
+def guess_number():
+    retries = 0
+
+    while(True):
+        retries += 1 
+
+        question = int(input("Type your guess > "))
+        list = [1,2,3,4,5,6,7,8,9,10]
+        result = random.choice(list) 
+        print(result)
+        
+        if retries == 2:
+            print("You have still one shot!!")
+        elif retries == 3:
+            print("You are out of tries")
+            print ("Game over!")
+            break;
+        elif question != result :   
+            print("Try again.." .format(question))
+        elif question == result:
+            print( "You guessed the correct number ".format(question))
+            exit(0)
+guess_number()
