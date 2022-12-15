ec45492da7876d9b8724c4a1a9cbd0d1f9b31071
Paris Charalampidis <curlycode@outlook.com>
2022-12-15 22:08:27 +0200

    adding more notes into the documentation

--- basic python exersices/variables.py
+++ basic python exersices/variables.py
@@ -29,3 +29,62 @@ print ("They will be ", cars_not_driven, "empty cars today.")
 print ("We can transport ", carpool_capacity, "people today.")
 print ("We have ", passengers, "to carpool today")
 print("We need to put about ", average_passengers_per_car, "in each car.")
+
+#global variables
+
+
+# numberic
+var = 123
+print("Numeric data : ", var)
+ 
+# Sequence Type
+String1 = 'Welcome to the Geeks World'
+print("String with the use of Single Quotes: ")
+print(String1)
+ 
+# Boolean
+print(type(True))
+print(type(False))
+ 
+# Creating a Set with
+# the use of a String
+set1 = set("GeeksForGeeks")
+print("\nSet with the use of String: ")
+print(set1)
+ 
+# Creating a Dictionary
+# with Integer Keys
+Dict = {1: 'Geeks', 2: 'For', 3: 'Geeks'}
+print("\nDictionary with the use of Integer Keys: ")
+print(Dict)
+
+#global variable
+
+# Python program to modify a global
+# value inside a function
+ 
+x = 15
+def change():
+ 
+    # using a global keyword
+    global x
+ 
+    # increment value of a by 5
+    x = x + 5
+    print("Value of x inside a function :", x)
+ 
+ 
+change()
+print("Value of x outside a function :", x)
+
+
+
+#How does + operator work with variables? 
+
+a = 10
+b = 20
+print(a+b)
+ 
+a = "Geeksfor"
+b = "Geeks"
+print(a+b)
