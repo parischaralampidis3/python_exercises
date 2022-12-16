139afede6c7399d167b8dd54e4a556d3b48dfb50
Paris Charalampidis <curlycode@outlook.com>
2022-12-17 01:09:39 +0200

    learning about classes in Python (vol.2)

--- basic python exersices/number_conversion.py
+++ basic python exersices/number_conversion.py
@@ -4,3 +4,43 @@ b = 5
 
 c = a + b 
 print(c)
+
+
+#convert to float
+a = 2 
+
+print ( float(a))
+
+
+#convert to intenger
+b =  5.6
+
+print( int(b))
+
+
+#print type of intenger
+
+
+c = '3'
+
+print(type(int(c)))
+
+
+#print type of float
+
+d = '5.6'
+
+print(type( float(d)))
+
+
+#print comnplex 
+e = 5
+print ( complex (e))
+
+#print  complex
+
+f = 6.5
+print(complex(f))
+
+
+
--- intermediate python exercises/classes.py
+++ intermediate python exercises/classes.py
@@ -14,3 +14,19 @@ fruit_salad = Fruit("apple")
 
 result.basket_of_fruits()
 fruit_salad.basket_of_fruits()
+
+
+class Dog:
+
+    attr1 = "mammal"
+    attr2 = "dog"
+
+    def fun(self):
+
+        print("I'm a ", self.attr1)
+        print("I'm a ", self.attr2) 
+
+Rodger = Dog()
+
+print( Rodger.attr1)
+Rodger.fun()
