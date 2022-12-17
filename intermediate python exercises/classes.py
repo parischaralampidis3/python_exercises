9a8f7dcbfc44f22055ac1aa08686b5e905f38cbc
Paris Charalampidis <curlycode@outlook.com>
2022-12-17 19:23:47 +0200

    learning about classes in Python (vol.3)

--- intermediate python exercises/classes.py
+++ intermediate python exercises/classes.py
@@ -16,6 +16,8 @@ result.basket_of_fruits()
 fruit_salad.basket_of_fruits()
 
 
+
+"""
 class Dog:
 
     attr1 = "mammal"
@@ -30,3 +32,47 @@ Rodger = Dog()
 
 print( Rodger.attr1)
 Rodger.fun()
+
+"""
+"""   
+class Person(object):
+  
+    def __init__(self, name):
+        self.name = name
+    
+    def say_hi(self):
+        print(self.name)
+    
+
+
+name1 = Person("Paris")
+
+name1.say_hi()
+
+"""
+
+class Person(object):
+  
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    
+    def say_hi(self):
+        print("My name is", self.name, "and my age is", self.age)
+
+
+info = Person("Paris", 38)
+
+info2  = Person("Valentina","25")
+
+info.say_hi()
+info2.say_hi()
+
+
+
+
+
+
+
+
+
